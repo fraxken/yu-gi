9a6595207bf0200ab6fe57eb553e6fd1823dac89
QuentinLpy <quentin.lepateley@gmail.com>
2021-04-06 00:22:33 +0200

    fix build and create images folder

--- build.js
+++ build.js
@@ -1,37 +1,45 @@
 // Import Node.js Dependencies
 const path = require("path");
 const fs = require("fs");
+const fse = require("fs-extra");
 
 // Import Third-party Dependencies
 const esbuild = require("esbuild");
 
 // CONSTANTS
 const kAssetsDir = path.join(__dirname, "assets");
+const kImageDir = path.join(kAssetsDir, "images");
 const kOutDir = path.join(__dirname, "out");
 
-fs.mkdirSync(kOutDir, { recursive: true });
+fse.mkdirSync(kOutDir, { recursive: true });
+
 
 async function main() {
-    await esbuild.build({
-        entryPoints: [path.join(kAssetsDir, "scripts", "main.js")],
-        loader: {
-            ".jpg": "file",
-            ".png": "file",
-            ".woff": "file",
-            ".woff2": "file",
-            ".eot": "file",
-            ".ttf": "file",
-            ".svg": "file",
-            ".json": "file"
-        },
-        platform: "browser",
-        bundle: true,
-        sourcemap: true,
-        treeShaking: true,
-        outdir: kOutDir
-    });
+  await esbuild.build({
+    entryPoints: [path.join(kAssetsDir, "scripts", "main.js")],
+    loader: {
+      ".jpg": "file",
+      ".png": "file",
+      ".woff": "file",
+      ".woff2": "file",
+      ".eot": "file",
+      ".ttf": "file",
+      ".svg": "file",
+      ".json": "file"
+    },
+    platform: "browser",
+    bundle: true,
+    sourcemap: true,
+    treeShaking: true,
+    outdir: kOutDir
+  });
 
-    fs.copyFileSync(path.join(kAssetsDir, "favicon.ico"), path.join(kOutDir, "favicon.ico"))
-    fs.copyFileSync(path.join(__dirname, "editor.html"), path.join(kOutDir, "index.html"));
+  //Copy specific file
+  fse.copyFileSync(path.join(kAssetsDir, "favicon.ico"), path.join(kOutDir, "favicon.ico"));
+  fse.copyFileSync(path.join(__dirname, "editor.html"), path.join(kOutDir, "index.html"));
+
+  //Copy images folder
+  fse.copy(kImageDir, path.join(kOutDir, 'images'));
 }
+
 main().catch(() => process.exit(1));
