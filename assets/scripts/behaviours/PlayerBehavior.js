05b8a0a8f47e9526fda534b0f0a8d343cec8c9b8
QuentinLpy <quentin.lepateley@gmail.com>
2021-04-11 22:21:50 +0200

    add life bar test

--- assets/scripts/behaviours/PlayerBehavior.js
+++ assets/scripts/behaviours/PlayerBehavior.js
@@ -11,68 +11,70 @@ import * as EntityBuilder from "../helpers/entitybuilder.js";
 import { Key } from "../helpers/input.class";
 
 const PlayerState = {
-    hp: "player.hp"
+  currentHp: "player.currentHp",
+  maxHp: "player.maxHp"
 };
 
 export default class PlayerBehavior extends ScriptBehavior {
-    constructor(speed = 2.5) {
-        super();
+  constructor(speed = 2.5, currentHp = 1, maxHp = 20) {
+    super();
 
-        this.speed = speed;
-        this.hp = 1;
-        this.time = new Timer(120);
+    this.speed = speed;
+    this.currentHp = currentHp;
+    this.maxHp = maxHp;
+    this.time = new Timer(60);
 
-        this.stateConfiguration(PlayerState);
+    this.stateConfiguration(PlayerState);
+  }
+
+  cameraInit() {
+    console.log("camera initialized");
+  }
+
+  awake() {
+    this.actor.addSprite(
+      new AnimatedSpriteEx("adventurer", { defaultAnimation: "adventurer-idle" })
+    );
+
+    console.log("sprite width: ", this.sprite.width);
+    console.log("sprite height: ", this.sprite.height);
+
+    // this.actor.y = (game.app.stage.height / 2) - (this.actor.sprite.height / 2);
+    // this.actor.x = (game.app.stage.width / 2) - (this.actor.sprite.width / 2);
+
+    game.viewport.follow(this.actor, {
+      speed: 1,
+      acceleration: 0.01,
+      radius: 40
+    });
+  }
+
+  update() {
+    if (this.time.walk() && this.currentHp < this.maxHp) {
+      this.currentHp += 1;
     }
 
-    cameraInit() {
-        console.log("camera initialized");
+    if (game.input.isKeyDown(Key.Q)) {
+      this.actor.moveX(-this.speed);
+      this.sprite.scale.x = -1;
+    }
+    else if (game.input.isKeyDown(Key.D)) {
+      this.actor.moveX(this.speed);
+      this.sprite.scale.x = 1;
     }
 
-    awake() {
-        this.actor.addSprite(
-            new AnimatedSpriteEx("adventurer", { defaultAnimation: "adventurer-idle" })
-        );
-
-        console.log("sprite width: ", this.sprite.width);
-        console.log("sprite height: ", this.sprite.height);
-
-        // this.actor.y = (game.app.stage.height / 2) - (this.actor.sprite.height / 2);
-        // this.actor.x = (game.app.stage.width / 2) - (this.actor.sprite.width / 2);
-
-        game.viewport.follow(this.actor, {
-            speed: 1,
-            acceleration: 0.01,
-            radius: 40
-        });
+    if (game.input.isKeyDown(Key.Z)) {
+      this.actor.moveY(-this.speed);
+    }
+    else if (game.input.isKeyDown(Key.S)) {
+      this.actor.moveY(this.speed);
     }
 
-    update() {
-        if (this.time.walk()) {
-            this.hp += 1;
-        }
-
-        if (game.input.isKeyDown(Key.Q)) {
-            this.actor.moveX(-this.speed);
-            this.sprite.scale.x = -1;
-        }
-        else if (game.input.isKeyDown(Key.D)) {
-            this.actor.moveX(this.speed);
-            this.sprite.scale.x = 1;
-        }
-
-        if (game.input.isKeyDown(Key.Z)) {
-            this.actor.moveY(-this.speed);
-        }
-        else if (game.input.isKeyDown(Key.S)) {
-            this.actor.moveY(this.speed);
-        }
-
-        this.sprite.playAnimation(this.hasVelocity ? "adventurer-run" : "adventurer-idle");
-    }
+    this.sprite.playAnimation(this.hasVelocity ? "adventurer-run" : "adventurer-idle");
+  }
 }
 
 EntityBuilder.define("actor:player", () => {
-    return new Actor("player")
-        .addScriptedBehavior(new PlayerBehavior());
+  return new Actor("player")
+    .addScriptedBehavior(new PlayerBehavior());
 });
---
+++ public/components/player-hp.js
@@ -0,0 +1,61 @@
+import { LitElement, css, html } from "lit-element";
+
+import { bindToScriptEvent } from "../helpers";
+
+class PlayerHp extends LitElement {
+  // static get properties() {
+  //   return {};
+  // }
+
+  constructor() {
+    super();
+    bindToScriptEvent(this, "player.currentHp", "currentHp");
+    bindToScriptEvent(this, "player.maxHp", "maxHp");
+  }
+
+  static get styles() {
+    return css`
+      .hud-life {
+        position: absolute;
+        bottom: 0;
+       }
+
+      .life-bar {
+        padding: 2px;
+        margin: 4px;
+        background-color: white;
+        border: 1px grey solid;
+        height: 20px;
+        border-radius: 3px;
+        width: 200px;
+       }
+
+      .current-life {
+        height: 100%;
+        background-color: red;
+       }
+
+       .info-life {
+         position: absolute;
+         color: white;
+         height: 20px;
+         padding: inherit;
+       }
+     `;
+  }
+
+  render() {
+    const lifePercent = (this.currentHp / this.maxHp) * 100;
+
+    return html`
+      <div class="hud-life">
+        <div class="life-bar">
+          <span class="info-life">${lifePercent} %</span>
+          <div class="current-life" style="width: ${lifePercent}% "/>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define("player-hp", PlayerHp);
