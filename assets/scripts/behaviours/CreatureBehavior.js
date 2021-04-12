e56ccd73f7204cbb7b04ddf61a2cbf9fb674989c
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-04-12 21:21:44 +0200

    feat(creaturebehavior): added creature with a fixed position & random deplacements

--- assets/scripts/behaviours/CreatureBehavior.js
+++ assets/scripts/behaviours/CreatureBehavior.js
@@ -12,59 +12,79 @@ import { Key } from "../helpers/input.class";
 
 const creatureRange = {
   position: {
-    y: 100,
-    x: 200
+    y: 50,
+    x: 50
   },
-  minY: -100,
-  maxY: 80,
-  minX: 20,
-  maxX: 100
+  radius: 20
 }
 
 export default class CreatureBehavior extends ScriptBehavior {
+
     constructor() {
         super();
 
-        this.lastAction = null;
+        this.nextPos = {
+            x: null,
+            y: null
+        };
+        this.isInAction = false;
+        this.action = null;
         this.time = new Timer(60);
     }
 
     awake() {
         this.actor.addSprite(
             new AnimatedSpriteEx("adventurer", { defaultAnimation: "adventurer-idle" })
-        );
-
-        console.log("Creature test sprite width: ", this.sprite.width);
-        console.log("Creature sprite height: ", this.sprite.height);
+            );
 
         this.actor.y = creatureRange.position.y;
         this.actor.x = creatureRange.position.x;
     }
 
     update() {
-        if (this.actor.y === creatureRange.position.y && this.actor.x === creatureRange.position.x) {
-          this.lastAction = "start";
+        if (!this.isInAction) {
+            if (!this.action) {
+                this.isInAction = true;
+                this.action = "DEPLACEMENT";
+                const r = creatureRange.radius * Math.sqrt(Math.random())
+                const theta = Math.random() * 2 * Math.PI
+                const x = Math.round(creatureRange.position.x + r * Math.cos(theta));
+                const y = Math.round(creatureRange.position.y + r * Math.sin(theta));
+
+                this.nextPos.x = x;
+                this.nextPos.y = y;
+                this.actor.y = this.actor.y < y ? this.actor.y +1: this.actor.y -1;
+                this.actor.x = this.actor.x < x ? this.actor.x +1: this.actor.x -1;
+            }
+            else {
+                if (this.time.walk()) {
+                    this.isInAction = true;
+                    this.action = "DEPLACEMENT";
+                    const r = creatureRange.radius * Math.sqrt(Math.random())
+                    const theta = Math.random() * 2 * Math.PI
+                    const x = Math.round(creatureRange.position.x + r * Math.cos(theta));
+                    const y = Math.round(creatureRange.position.y + r * Math.sin(theta));
+
+                    this.nextPos.x = x;
+                    this.nextPos.y = y;
+                    this.actor.y = this.actor.y < y ? this.actor.y +1: this.actor.y -1;
+                    this.actor.x = this.actor.x < x ? this.actor.x +1: this.actor.x -1;
+                }
+            }
         }
-        
-        if (this.lastAction === "start" || this.lastAction === "getUp") {
-          if (this.actor.y !== creatureRange.minY) {
-            this.actor.y = this.actor.y > creatureRange.minY ? this.actor.y -1: this.actor.y +1;
-          }
-
-          if (this.actor.y === creatureRange.minY) {
-            this.lastAction = "getDown";
-          }
-        }
-
-        if (this.lastAction === "getDown") {
-          console.log("up");
-          if (this.actor.y !== creatureRange.maxY) {
-            this.actor.y = this.actor.y < creatureRange.maxY ? this.actor.y +1: this.actor.y -1;
-          }
-
-          if (this.actor.y === creatureRange.maxY) {
-            this.lastAction = "getUp";
-          }
+        else {
+            if (this.action === "DEPLACEMENT") {
+                if (this.nextPos.x === this.actor.x && this.nextPos.y === this.actor.y) {
+                    this.action = null;
+                    this.nextPos.x = null;
+                    this.nextPos.y = null;
+                    this.isInAction = false;
+                }
+                else {
+                    if (this.nextPos.x !== this.actor.x) this.actor.x = this.actor.x < this.nextPos.x ? this.actor.x +1: this.actor.x -1;
+                    if (this.nextPos.y !== this.actor.y) this.actor.y = this.actor.y < this.nextPos.y ? this.actor.y +1: this.actor.y -1;
+                }
+            }
         }
 
         this.sprite.playAnimation(this.hasVelocity ? "adventurer-run" : "adventurer-idle");
