40e7402aecdf043afd89322c3a84ab8251ae4dfc
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-04-13 21:27:37 +0200

    feat(helpers): added export for easing functions

--- assets/scripts/helpers/index.js
+++ assets/scripts/helpers/index.js
@@ -1,10 +1,12 @@
 import Keyboard, { Key } from "./input.class";
 import * as EntityBuilder from "./entitybuilder";
+import * as EasingFunction from "./easing";
 import Timer from "./timer.class";
 
 export {
     Keyboard,
     Key,
     EntityBuilder,
+    EasingFunction,
     Timer
 }
