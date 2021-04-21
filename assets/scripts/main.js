3df252d66d4dfb798e6d68bdae003201f8ecde9d
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-04-21 18:56:54 +0200

    refactor: work on PIXI.font and font loading

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,12 @@
 // Import third-party Dependencies
 import { filters } from "@pixi/sound";
 import * as PIXI from "pixi.js";
+// import FontFaceObserver from "fontfaceobserver";
 window.PIXI = PIXI;
+
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+PIXI.settings.RESOLUTION = 2;
+PIXI.settings.PRECISION_FRAGMENT = "highp";
 
 // Import ECS & Helpers Dependencies
 import { State, Engine } from "./ECS";
@@ -14,33 +18,41 @@ import { DungeonScene, DefaultScene } from "./scenes";
 
 import assetsURL from "../assets.json";
 
-const gameState = new State("gameState", {
-    player: {
-        name: "Thomas",
-        currentHp: 1,
-        maxHp: 15
-    }
-});
+async function main() {
+    const gameState = new State("gameState", {
+        player: {
+            name: "Thomas",
+            currentHp: 1,
+            maxHp: 15
+        }
+    });
 
-const game = new Engine({ defaultScene: DefaultScene, state: gameState })
-    .loadAssetFromFile(assetsURL)
-    // .registerTileSet("TilesetFloorB")
-    .init();
+    const game = new Engine({ defaultScene: DefaultScene, state: gameState })
+        .loadAssetFromFile(assetsURL)
+        // .registerTileSet("TilesetFloorB")
+        .init();
 
-// new BackgroundMediaPlayer({
-//     defaultTrack: "default",
-//     defaultFilters: [
-//         new filters.DistortionFilter(0.05),
-//         new filters.ReverbFilter(1, 9)
-//     ],
-//     tracks: {
-//         default: [
-//             { name: "ambient-sound", volume: 0.025 },
-//             { name: "ambient-void", volume: 0.025 }
-//         ]
-//     }
-// }).bindToEngine(game);
+    // new BackgroundMediaPlayer({
+    //     defaultTrack: "default",
+    //     defaultFilters: [
+    //         new filters.DistortionFilter(0.05),
+    //         new filters.ReverbFilter(1, 9)
+    //     ],
+    //     tracks: {
+    //         default: [
+    //             { name: "ambient-sound", volume: 0.025 },
+    //             { name: "ambient-void", volume: 0.025 }
+    //         ]
+    //     }
+    // }).bindToEngine(game);
 
-game.on("awake", () => {
-    loadHUD("test_hud");
-});
+    game.on("awake", () => {
+        loadHUD("test_hud");
+    });
+}
+main().catch(console.error);
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const font = new FontFaceObserver("Roboto");
+//     font.load().then(main).catch(console.error);
+// });
