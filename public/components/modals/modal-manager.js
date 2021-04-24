8c90476bf9d953f7b143c585087cc2606939616d
tony-go <gorez.tony@gmail.com>
2021-04-24 09:44:45 +0200

    feat: add style to modal manager

--- public/components/modals/modal-manager.js
+++ public/components/modals/modal-manager.js
@@ -1,8 +1,24 @@
-import { LitElement, html } from "lit-element";
+import { LitElement, html, css } from "lit-element";
 
 class ModalManager extends LitElement {
   constructor() {
     super();
+  }
+
+  static get styles() {
+    return css`
+      .modal {
+        pointer-events: all;
+        font-family: "Helvetica";
+        background-color: rgba(0, 0, 0, 0.3);
+        padding: 20px;
+        position: absolute;
+        height: 80%; 
+        width: 80%; 
+        top: 10%; 
+        left: 10%; 
+      }
+    `
   }
 
   render() {
