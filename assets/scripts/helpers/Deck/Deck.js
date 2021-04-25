4541a65d18512147e972568545f3c24262f333be
Cedric Lionnet <c.lionnet@myunisoft.fr>
2021-04-25 10:15:39 +0200

    Deck logic
    - Recuperator max 5 cards, least number of stars deleted,
        oldest if equality
    - Events send to HUD on card use and carousel

--- assets/scripts/helpers/Deck/Deck.js
+++ assets/scripts/helpers/Deck/Deck.js
@@ -1,13 +1,19 @@
 import { Card } from "./Card";
 
-const GameCards = Object.freeze([
-    new Card("First Card", function () { console.log("sword") }, "shield", "speedBoost", "nuke", 2),
+const StarterCards = Object.freeze([
+    new Card("First Card", function () { console.log("sword") }, "shield", "speedBoost", "nuke", 1),
     new Card("Second Card", function () { console.log("bow") }, "sideDodge", "speedBoost", "nuke", 3),
     new Card("Third Card", function () { console.log("spear") }, "backDodge", "speedBoost", "nuke", 2),
     new Card("Fourth Card", function () { console.log("axe") }, "roll", "speedBoost", "nuke", 4),
-    new Card("Fifth Card", function () { console.log("hammer") }, "parry", "speedBoost", "nuke", 2),
-    new Card("Sixth Card", function () { console.log("hammer") }, "roll", "speedBoost", "nuke", 2),
-    new Card("Seventh Card", function () { console.log("axe") }, "shield", "speedBoost", "nuke", 2)
+    new Card("Fifth Card", function () { console.log("crossbow") }, "parry", "speedBoost", "nuke", 2),
+    new Card("Sixth Card", function () { console.log("hammer") }, "roll", "speedBoost", "nuke", 2)
+]);
+
+const AdditionalCards = Object.freeze([
+    new Card("Seventh Card", function () { console.log("knife") }, "shield", "speedBoost", "nuke", 2),
+    new Card("Eighth Card", function () { console.log("crossbow") }, "shield", "speedBoost", "nuke", 2),
+    new Card("Nineth Card", function () { console.log("wand") }, "shield", "speedBoost", "nuke", 2),
+    new Card("Tenth Card", function () { console.log("spear") }, "shield", "speedBoost", "nuke", 2)
 ]);
 
 const SkillIndex = Object.freeze({
@@ -18,21 +24,43 @@ const SkillIndex = Object.freeze({
 });
 
 const MAK_NUMBER_OF_CARDS_IN_SLOT_HUD = 4;
+const MAX_NUMBER_OF_CARDS_IN_RECUPERATOR = 5;
 
 export class Deck {
     constructor() {
         this.discardedCards = [];
 
-        this.cardDraw = Array.from(GameCards);
+        this.cardDraw = Array.from(StarterCards);
         this.shuffleCardDraw();
 
+        this.lockedCard = [];
         this.recuperator = [];
 
         this.slotHUD = [];
         this.initSlotHUD();
+
+        this.debugLog();
+    }
+
+    loadIntoHUD() {
+        window.hudevents.emit("offensive_card", this.slotHUD[0]);
+        window.hudevents.emit("defensive_card", this.slotHUD[1]);
+        window.hudevents.emit("passive_card", this.slotHUD[2]);
+        window.hudevents.emit("consomable_card", this.slotHUD[3]);
+    }
+
+    updateRecuperator() {
+        this.recuperator.push(...this.lockedCard);
+
+        while (this.recuperator.length > MAX_NUMBER_OF_CARDS_IN_RECUPERATOR) {
+            const minNumberOfStars = Math.min.apply(Math, this.recuperator.map(card => card.stars));
+            const indexToRemove = this.recuperator.findIndex(card => card.stars === minNumberOfStars);
+            this.recuperator.splice(indexToRemove, 1);
+        }
     }
 
     shuffleCardDraw() {
+        // Fisher-Yates shuffle
         for (let i = this.cardDraw.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [this.cardDraw[i], this.cardDraw[j]] = [this.cardDraw[j], this.cardDraw[i]];
@@ -63,7 +91,14 @@ export class Deck {
     }
 
     dump(card) {
-        this.recuperator.push(card);
+        this.lockedCard.push(card);
+    }
+
+    recover(cardIndex) {
+        const cardToRecover = this.lockedCard.splice(cardIndex, 1);
+        if (cardToRecover) {
+            this.cardDraw.unshift(cardToRecover);
+        }
     }
 
     useOffensiveSkill() {
@@ -73,6 +108,10 @@ export class Deck {
 
         this.slotHUD.splice(SkillIndex.Offensive, 1);
         this.slotHUD.splice(SkillIndex.Offensive, 0, this.pick());
+
+        window.hudevents.emit("offensive_card", this.slotHUD[0]);
+
+        this.debugLog();
     }
 
     useDefensiveSkill() {
@@ -80,6 +119,10 @@ export class Deck {
 
         this.slotHUD.splice(SkillIndex.Defensive, 1);
         this.slotHUD.splice(SkillIndex.Defensive, 0, this.pick());
+
+        window.hudevents.emit("defensive_card", this.slotHUD[1]);
+
+        this.debugLog();
     }
 
     useConsumable() {
@@ -89,6 +132,10 @@ export class Deck {
             this.slotHUD.splice(SkillIndex.Consumable, 1);
             this.slotHUD.splice(SkillIndex.Consumable, 0, this.pick());
         }
+
+        window.hudevents.emit("consomable_card", this.slotHUD[3]);
+
+        this.debugLog();
     }
 
     carouselSlot() {
@@ -97,6 +144,22 @@ export class Deck {
             this.slotHUD[0],
             this.slotHUD[1],
             this.slotHUD[2]
-        ]); // LOOKS SHITTY
+        ]);
+
+        window.hudevents.emit("offensive_card", this.slotHUD[0]);
+        window.hudevents.emit("defensive_card", this.slotHUD[1]);
+        window.hudevents.emit("passive_card", this.slotHUD[2]);
+        window.hudevents.emit("consomable_card", this.slotHUD[3]);
+
+        this.debugLog();
+    }
+
+    debugLog() {
+        if (true) {
+            console.log("DISCARDED CARDS:", this.discardedCards);
+            console.log("CARDDRAW:", this.cardDraw);
+            console.log("HUD SLOTS:", this.slotHUD);
+            console.log("RECUPERATOR:", this.lockedCard);
+        }
     }
 }
