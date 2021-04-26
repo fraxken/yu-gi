7102fe7ee5657b5ad79629282ffee09d262b6f0e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-04-26 14:35:32 +0200

    chore: prototype of chest

--- assets/scripts/behaviours/ChestBehavior.js
+++ assets/scripts/behaviours/ChestBehavior.js
@@ -22,15 +22,10 @@ export default class ChestBehavior extends ScriptBehavior {
         }
 
         this.opened = false;
-        this.destroyOnClose = false;
     }
 
     start() {
         this.target = getActor("player");
-    }
-
-    remove() {
-        this.actor.cleanup();
     }
 
     destroy() {
@@ -50,13 +45,15 @@ export default class ChestBehavior extends ScriptBehavior {
         hudevents.emit("trunk", false);
         this.opened = false;
         window.trunkCards = null;
-
-        if (this.destroyOnClose) {
-            this.remove();
-        }
     }
 
     update() {
+        if (this.cards.length === 0) {
+            this.actor.cleanup();
+            hudevents.emit("trunk", false);
+            return;
+        }
+
         const distance = this.actor.pos.distanceTo(this.target.pos);
         const inArea = distance < ChestBehavior.DistanceToOpen;
 
--- public/components/modals/trunk/index.js
+++ public/components/modals/trunk/index.js
@@ -1,4 +1,5 @@
 import { css, LitElement, html } from 'lit-element';
+import { Card } from '../../../../assets/scripts/helpers/Deck/Card';
 
 import { buildFakeCards } from '../../../fixtures';
 
@@ -6,20 +7,48 @@ class TrunkModal extends LitElement {
     static get styles() {
         return css`
             .modal-trunk {
-                padding: 20px;
-                border: 5px solid red;
+                padding: 60px;
+                background: #474747;
+                background: -moz-linear-gradient(top,  #474747 0%, #1e1812 100%);
+                background: -webkit-linear-gradient(top,  #474747 0%,#1e1812 100%);
+                background: linear-gradient(to bottom,  #474747 0%,#1e1812 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#474747', endColorstr='#1e1812',GradientType=0 );
+                border: 4px solid black;
                 border-radius: 10px;
                 display: flex;
             }
             .modal-trunk .card-slot {
-                width: 200px;
-                height: 200px;
-                background: red;
                 display: flex;
-                flex-direction: column;sq
+                flex-direction: column;
             }
             .modal-trunk .card-slot + .card-slot {
-                margin-left: 20px;
+                margin-left: 80px;
+            }
+            .modal-trunk .card-slot .pick {
+                height: 40px;
+                background: #95a5a5;
+                background: -moz-linear-gradient(top,  #95a5a5 0%, #596a72 100%);
+                background: -webkit-linear-gradient(top,  #95a5a5 0%,#596a72 100%);
+                background: linear-gradient(to bottom,  #95a5a5 0%,#596a72 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#95a5a5', endColorstr='#596a72',GradientType=0 );
+                color: black;
+                box-sizing: border-box;
+                border-radius: 10px;
+                border: 2px solid black;
+                padding: 0 20px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-family: Roboto;
+                margin-top: 20px;
+                font-weight: bold;
+                letter-spacing: 1px;
+            }
+            .modal-trunk .card-slot .pick:hover {
+                cursor: pointer;
+                border-color: #FFF;
+                color: #FFF;
+                text-shadow: 2px 2px 2px black;
             }
         `;
     }
@@ -27,17 +56,29 @@ class TrunkModal extends LitElement {
     constructor() {
         super();
 
-        console.log(window.trunkCards);
+        /** @type {Card[]} */
         this.cards = Array.isArray(window.trunkCards) ? window.trunkCards : buildFakeCards(3);
     }
 
-    *getCards() {
-        for (const card of this.cards) {
+    addCardToDeck(event) {
+        const id = event.target.getAttribute("data-id");
+        const [card] = this.cards.splice(id, 1);
+        console.log(card);
+        // TODO: add card to the deck!
+
+        this.update();
+    }
+
+    *fetchCardsInCurrentChest() {
+        for (let id = 0; id < this.cards.length; id++) {
+            const card = this.cards[id];
+            // Type of the card ?? typeCard
             console.log(card);
 
             yield html`
             <div class="card-slot">
-                <render-slot .card='${card}' name='boo' typeCard='offensive'></render-slot>
+                <render-slot .card='${card}' typeCard='offensive'></render-slot>
+                <div class="pick" data-id=${id} @click=${this.addCardToDeck}>ADD TO DECK</div>
             </div>
             `;
         }
@@ -45,7 +86,7 @@ class TrunkModal extends LitElement {
 
     render() {
         return html`
-            <div class="modal-trunk">${[...this.getCards()].concat()}</div>
+            <div class="modal-trunk">${[...this.fetchCardsInCurrentChest()].concat()}</div>
         `;
     }
 }
